c4cf8c01de7c417340fee58b7359d0e7e2320632
Damian Fanaro <damianfanaro@gmail.com>
2023-07-30 22:48:32 +0200

    Fix style

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import GenreList from "./components/GenreList";
 
 function App() {
     return (
-        <Grid templateAreas={{ base: `"nav" "main" "footer"`, lg: `"nav nav" "aside main" "footer footer"` }}>
+        <Grid
+            templateAreas={{ base: `"nav" "main" "footer"`, lg: `"nav nav" "aside main" "footer footer"` }}
+            templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+        >
             <GridItem area="nav">
                 <NavBar />
             </GridItem>
             <Show above="lg">
-                <GridItem area="aside">
+                <GridItem area="aside" paddingX={5}>
                     <GenreList />
                 </GridItem>
             </Show>
